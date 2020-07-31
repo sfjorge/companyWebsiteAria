3b6fa0514d169d22d4b4c455635ee044a16d79bf
eduardo_m <eduardombiotech@gmail.com>
2020-07-31 09:59:21 -0400

    added email to form and changed title

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,8 +25,8 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Aria landing page";
+$EmailTo = "jorgesantos214@gmail.com";
+$Subject = "New message from Mulet Internation Website";
 
 // prepare email body text
 $Body = "";
